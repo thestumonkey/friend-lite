5e6047ec936850c3e20084e5a15623e5c251504b
Stu Alexandere <thestumonkey@gmail.com>
2025-11-18 10:28:29 +0000

    fixed incorrect param

--- backends/advanced/src/advanced_omi_backend/client_manager.py
+++ backends/advanced/src/advanced_omi_backend/client_manager.py
@@ -104,7 +104,7 @@ class ClientManager:
         """
         return len(self._active_clients)
 
-    def create_client(self, client_id: str, ac_repository, chunk_dir, user_id: str, user_email: Optional[str] = None) -> "ClientState":
+    def create_client(self, client_id: str, chunk_dir, user_id: str, user_email: Optional[str] = None) -> "ClientState":
         """
         Atomically create and register a new client.
 
@@ -113,7 +113,6 @@ class ClientManager:
 
         Args:
             client_id: Unique client identifier
-            ac_repository: Audio chunks repository
             chunk_dir: Directory for audio chunks
             user_id: User ID who owns this client
             user_email: Optional user email
